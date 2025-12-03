73c5cd719d8793cba99836827111493d23a1d7e8
Thiago william <thiago.will.silva@gmail.com>
2025-12-03 19:10:28 -0300

    feat: Upload de arquivos para o R2

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -34,6 +34,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       })
 
       if (isRight(result)) {
+        console.log(unwrapEither(result))
         return reply.status(201).send()
       }
 
